2fbb3974beb3779ef80e77fff2662799d098b501
Lu Nelson <lunelson@gmail.com>
2025-08-09 12:22:42 +0200

    finish the last task
    Signed-off-by: Lu Nelson <lunelson@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_char_counts, get_char_count_dicts
+import sys
 
 
 def get_book_text(file_path):
@@ -8,13 +9,16 @@ def get_book_text(file_path):
 
 
 def main():
-    path = "books/frankenstein.txt"
-    book_text = get_book_text(path)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_book = sys.argv[1]
+    book_text = get_book_text(path_to_book)
     num_words = get_num_words(book_text)
     char_counts = get_char_counts(book_text)
     char_count_dicts = get_char_count_dicts(char_counts)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path}...")
+    print(f"Analyzing book found at {path_to_book}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
