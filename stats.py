fdcf46fe02e5b1527ba0c7ecd5ca34f7ff695df7
Lu Nelson <lunelson@gmail.com>
2025-08-09 10:45:32 +0200

    add typing
    Signed-off-by: Lu Nelson <lunelson@gmail.com>

--- stats.py
+++ stats.py
@@ -1,9 +1,18 @@
-def get_num_words(text):
+# pyright: strict
+from typing import TypedDict
+
+
+class CharCount(TypedDict):
+    char: str
+    num: int
+
+
+def get_num_words(text: str) -> int:
     return len(text.split())
 
 
-def get_char_counts(text):
-    char_counts = dict()
+def get_char_counts(text: str) -> dict[str, int]:
+    char_counts: dict[str, int] = dict()
     for char in text:
         lchar = char.lower()
         if lchar not in char_counts:
@@ -12,7 +21,9 @@ def get_char_counts(text):
     return char_counts
 
 
-def get_char_count_dicts(counts_dict):
-    char_count_dicts = [{"char": char, "num": num} for char, num in counts_dict.items()]
+def get_char_count_dicts(counts_dict: dict[str, int]) -> list[CharCount]:
+    char_count_dicts: list[CharCount] = [
+        {"char": char, "num": num} for char, num in counts_dict.items()
+    ]
     char_count_dicts.sort(key=lambda x: x["num"], reverse=True)
     return char_count_dicts
