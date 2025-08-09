366c1ced06300cee303ac7e65889e509cd160086
Lu Nelson <lunelson@gmail.com>
2025-08-09 10:27:32 +0200

    CH3 L1
    Signed-off-by: Lu Nelson <lunelson@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from stats import get_num_words, count_chars
+from stats import get_num_words, get_char_counts, get_char_count_dicts
+
 
 def get_book_text(file_path):
     with open(file_path) as f:
         content = f.read()
     return content
 
+
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    path = "books/frankenstein.txt"
+    book_text = get_book_text(path)
     num_words = get_num_words(book_text)
-    print(f"{num_words} words found in the document")
-    char_count = count_chars(book_text)
-    print(char_count)
+    char_counts = get_char_counts(book_text)
+    char_count_dicts = get_char_count_dicts(char_counts)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char_dict in char_count_dicts:
+        if char_dict["char"].isalpha():
+            print(f"{char_dict['char']}: {char_dict['num']}")
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,18 @@
 def get_num_words(text):
     return len(text.split())
 
-def count_chars(text):
-    char_dict = dict()
+
+def get_char_counts(text):
+    char_counts = dict()
     for char in text:
         lchar = char.lower()
-        if lchar not in char_dict:
-            char_dict[lchar] = 0
-        char_dict[lchar] += 1
-    return char_dict
+        if lchar not in char_counts:
+            char_counts[lchar] = 0
+        char_counts[lchar] += 1
+    return char_counts
+
+
+def get_char_count_dicts(counts_dict):
+    char_count_dicts = [{"char": char, "num": num} for char, num in counts_dict.items()]
+    char_count_dicts.sort(key=lambda x: x["num"], reverse=True)
+    return char_count_dicts
